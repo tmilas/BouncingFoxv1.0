1acdb2e73dfed5872dca907053a5d38043169cc5
tmilas <33846198+tmilas@users.noreply.github.com>
2021-05-14 19:16:00 +0300

    collectable implement

--- Assets/Scripts/CollectableItem.cs
+++ Assets/Scripts/CollectableItem.cs
@@ -9,6 +9,20 @@ public class CollectableItem : MonoBehaviour
         GravityIncrease2x, GravityIncrease3x, ObstacleIncrease2x,
         ObstacleIncrease3x,RandomCollectable }
 
-    public float bonusPoints = 0f;
+    public float itemFactor = 0f;
+    public float itemDuration = 3f;
     public CollectableType collectableType;
+
+    private GameEngine gameEngine;
+
+    private void Start()
+    {
+        gameEngine = FindObjectOfType<GameEngine>();    
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        gameEngine.SetGameBonus(this);
+    }
+
 }
