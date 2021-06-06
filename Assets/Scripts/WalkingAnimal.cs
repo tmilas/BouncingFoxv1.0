79d300f5c6f1c269d4f1792d13fb43d652a7f806
tmilas <33846198+tmilas@users.noreply.github.com>
2021-06-06 20:01:09 +0300

    levels created

--- Assets/Scripts/WalkingAnimal.cs
+++ Assets/Scripts/WalkingAnimal.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class WalkingAnimal : MonoBehaviour
 {
 
-    public float walkingSpeed=0.09f;
+    public float walkingSpeed=0.12f;
     private Vector2 animalPosition= new Vector2();
+
+    private GameEngine gameEngine;
 
     // Start is called before the first frame update
     void Start()
     {
         animalPosition = transform.position;
+        gameEngine = FindObjectOfType<GameEngine>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        animalPosition.x -= walkingSpeed;
+        animalPosition.x -= walkingSpeed * gameEngine.speedFactor;
         transform.position = animalPosition;
     }
 }
