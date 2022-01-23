90df5657654bd32f8e8b6bee12e7efe509bb1ef9
tolgakayar <31161017+tolgakayar@users.noreply.github.com>
2022-01-23 16:21:53 +0300

    life engine
    life engine singleton, limit updates
    languagesupport , storage engine singleton updates
    hide start game button when 0 lives remains.

--- Assets/Scripts/StartGameCanvas.cs
+++ Assets/Scripts/StartGameCanvas.cs
@@ -1,14 +1,63 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class StartGameCanvas : MonoBehaviour
 {
-    public Text startGameText;
+    #region life
+    public Text lifeText;
+    public Text timesLeftTexT;
+    public Button startButton;
+
+    private LifeEngine lifeEngine;
 
     void Start()
     {
-        //LanguageSupport langSupport = FindObjectOfType<LanguageSupport>();
+        timesLeftTexT.gameObject.SetActive(false);
 
-        //startGameText.text = langSupport.GetText("startnewgame");
+        lifeEngine = FindObjectOfType<LifeEngine>();
+
+        lifeEngine.SetActiveScene("start");
     }
+
+    public void ShowLife(string livesLeft)
+    {
+        lifeText.text = livesLeft;
+    }
+
+    public void ShowLifeCounter(TimeSpan timesLeft)
+    {
+        timesLeftTexT.text = timesLeft.Minutes.ToString("00") + ":" + timesLeft.Seconds.ToString("00");
+    }
+
+    public bool IsLifeTimesLeftActive()
+    {
+        if(!timesLeftTexT)
+        {
+            return false;
+        }
+
+        //Debug.Log(timesLeftTexT.IsActive());
+
+        return timesLeftTexT.IsActive();
+    }
+
+    public void SetLifeTimesLeftActive(bool active)
+    {
+        timesLeftTexT.gameObject.SetActive(active);
+    }
+
+    public void StartGame()
+    {
+        if(lifeEngine)
+        {
+            lifeEngine.DecideStart();
+        }
+    }
+
+    public void EnableStartButton(bool status)
+    {
+        startButton.gameObject.SetActive(status);
+    }
+    #endregion
 }
