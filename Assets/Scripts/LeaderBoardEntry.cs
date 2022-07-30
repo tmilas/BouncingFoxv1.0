575514b864798a0af49ed1ccae6fc4d4c9969ce5
tmilas <33846198+tmilas@users.noreply.github.com>
2022-07-30 16:19:41 +0300

    leaderboard own services changes

---
+++ Assets/Scripts/LeaderBoardEntry.cs
@@ -0,0 +1,8 @@
+public class LeaderBoardEntry
+{
+    //public int instanceId { get; set; }
+    public string gameid { get; set; }
+    public string playerid { get; set; }
+    public int playerscore { get; set; }
+
+}
