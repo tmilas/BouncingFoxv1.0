9579e56cff4475011e4f4fada28a5692c08de7ce
tmilas <33846198+tmilas@users.noreply.github.com>
2023-06-04 16:52:27 +0300

    IAP purchase relation implemented

--- Assets/Scripts/IAPEventHandler.cs
+++ Assets/Scripts/IAPEventHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,16 @@ using UnityEngine.UI;
 
 public class IAPEventHandler : MonoBehaviour
 {
+
+    private LifeEngine lifeEngine;
+
     public void UnlockGameGranted()
     {
         Debug.Log("Unlock Game Granted!!!");
+        lifeEngine = FindObjectOfType<LifeEngine>();
+        DateTime endDate = new DateTime(2099, 1, 1);
+        lifeEngine.NoLimitsUntil(endDate);
+
         GameObject highScoreText = GameObject.Find("HighScore Text");
         highScoreText.GetComponent<Text>().text = "Granted";
     }
